cb25d6036b0d9dc6ce44cdbee81487648e2afbc6
M-Phoenixx <mchuhan66@gmail.com>
2022-07-31 19:54:08 +0530

    responsive ui complete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const heart = document.getElementById('heart');
 const play = document.getElementById('play')
 const pause = document.getElementById('pause')
+const playbg = document.getElementById('playbg')
 let color = 0;
 heart.addEventListener('click',()=>{
     if(color==0){
@@ -10,5 +11,18 @@ heart.addEventListener('click',()=>{
     else {
         heart.style.color = "black";
         color = 0;
+    }
+})
+let paly = 0;
+playbg.addEventListener('click',()=>{
+    if(paly == 0){
+        pause.style.display = 'block'
+        play.style.display ='none'
+        paly =1;
+    }
+    else{
+        pause.style.display = 'none'
+        play.style.display ='block'
+        paly =0;
     }
 })
